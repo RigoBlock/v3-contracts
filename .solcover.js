81f2a40f931fa33de9dd2d94f3b2810171fdea0c
gab <gabriele.rigo@gmail.com>
2022-06-23 10:08:32 +0200

    add solcover.js optimizer settings
    - set additional parameters to prevent 'stack too deep' error since coverage does not use optimizer by default

--- .solcover.js
+++ .solcover.js
@@ -3,5 +3,16 @@ module.exports = {
     mocha: {
         grep: "@skip-on-coverage", // Find everything with this tag
         invert: true               // Run the grep's inverse set.
+    },
+    configureYulOptimizer: true,
+    solcOptimizerDetails: {
+      peephole: false,
+      inliner: false,
+      jumpdestRemover: false,
+      orderLiterals: true,
+      deduplicate: false,
+      cse: false,
+      constantOptimizer: false,
+      yul: false
     }
 };
