260c3279bb54ad3fa58ea9db33bdadd94df2055b
gab <gabriele.rigo@gmail.com>
2022-07-04 12:31:05 +0200

    add more factory tests

--- test/factory/RigoblockPoolProxyFactory.spec.ts
+++ test/factory/RigoblockPoolProxyFactory.spec.ts
@@ -19,25 +19,91 @@ describe("ProxyFactory", async () => {
 
     describe("createDrago", async () => {
         it('should revert with space before pool name', async () => {
-            const { factory, addresslog } = await setupTests()
-            // TODO: factory address should not change with deterministic deployment
-            console.log(addresslog)
+            const { factory } = await setupTests()
             await expect(
-                factory.createDrago(' testpool2', 'TEST2')
+                factory.createDrago(' testpool', 'TEST')
+            ).to.be.revertedWith("Transaction reverted without a reason")
+        })
+
+        it('should revert with space after pool name', async () => {
+            const { factory } = await setupTests()
+            await expect(
+                factory.createDrago('testpool ', 'TEST')
+            ).to.be.revertedWith("Transaction reverted without a reason")
+        })
+
+        it('should revert with special character in pool name', async () => {
+            const { factory, addresslog } = await setupTests()
+            await expect(
+                factory.createDrago('test+pool', 'TEST')
             ).to.be.revertedWith("Transaction reverted without a reason")
         })
 
         it('should revert with space before pool symbol', async () => {
             const { factory } = await setupTests()
             await expect(
-                factory.createDrago('testpool3', ' TEST3')
+                factory.createDrago('testpool2', ' TEST')
             ).to.be.revertedWith("Transaction reverted without a reason")
         })
 
+        // TODO: fix following, as poolAddress is tx hash, not the return value
         it('should create address when creating pool', async () => {
             const { factory } = await setupTests()
             const poolAddress = await factory.createDrago('testpool', 'TEST')
             expect(poolAddress).to.be.not.eq(AddressZero)
+        })
+
+        // TODO: fix following, as poolAddress is tx hash, not the return value
+        it('should create pool with space not first or last character', async () => {
+            const { factory } = await setupTests()
+            const poolAddress = await factory.createDrago('t est pool', 'TEST')
+            expect(poolAddress).to.be.not.eq(AddressZero)
+        })
+
+        // TODO: fix following, as poolAddress is tx hash, not the return value
+        it('should create pool with uppercase character in name', async () => {
+            const { factory } = await setupTests()
+            const poolAddress = await factory.createDrago('testPool', 'TEST')
+            expect(poolAddress).to.be.not.eq(AddressZero)
+        })
+
+        // TODO: following should revert in registry
+        it('should revert with duplicate name', async () => {
+            const { factory } = await setupTests()
+            await factory.createDrago('duplicateName', 'TEST')
+            await expect(
+                factory.createDrago('duplicateName', 'TEST')
+            ).to.be.revertedWith("PROXY_FACTORY_LIBRARY_DEPLOY_ERROR")
+        })
+
+        // TODO: fix following test
+        it('should create pool with duplicate symbol', async () => {
+            const { factory } = await setupTests()
+            await factory.createDrago('someName', 'TEST')
+            await expect(
+              factory.createDrago('someOtherName', 'TEST')
+            ).to.be.revertedWith("Transaction reverted without a reason")
+        })
+
+        it('should revert with symbol longer than 5 characters', async () => {
+            const { factory } = await setupTests()
+            await expect(
+                factory.createDrago('testpool2', 'TOOLONG')
+            ).to.be.revertedWith("Transaction reverted without a reason")
+        })
+
+        it('should revert with symbol SHORTER than 3 characters', async () => {
+            const { factory } = await setupTests()
+            await expect(
+                factory.createDrago('testpool2', 'TS')
+            ).to.be.revertedWith("Transaction reverted without a reason")
+        })
+
+        it('should revert with lowercase symbol', async () => {
+            const { factory } = await setupTests()
+            await expect(
+                factory.createDrago('testpool2', 'test')
+            ).to.be.revertedWith("Transaction reverted without a reason")
         })
     })
 })
