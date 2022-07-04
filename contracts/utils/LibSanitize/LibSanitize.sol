7127701d9526a17b4efdff2f956252d7c4df419d
gab <gabriele.rigo@gmail.com>
2022-07-04 18:16:31 +0200

    add rich errors in LibSanitize require checks

--- contracts/utils/LibSanitize/LibSanitize.sol
+++ contracts/utils/LibSanitize/LibSanitize.sol
@@ -31,11 +31,13 @@ library LibSanitize {
         bytes memory bStr = bytes(str);
         uint arrayLength = bStr.length;
         require(
-            bStr[0] != bytes1(uint8(32))
-        );    
+            bStr[0] != bytes1(uint8(32)),
+            "LIBSANITIZE_SPACE_AT_BEGINNING_ERROR"
+        );
         require(
-            bStr[arrayLength - 1] != bytes1(uint8(32))
-        );    
+            bStr[arrayLength - 1] != bytes1(uint8(32)),
+            "LIBSANITIZE_SPACE_AT_END_ERROR"
+        );
         for (uint i =0; i < arrayLength; i++) {
             if (
                 (
@@ -44,7 +46,7 @@ library LibSanitize {
                     bStr[i] > bytes1(uint8(57)) && bStr[i] < bytes1(uint8(65)) ||
                     bStr[i] > bytes1(uint8(90)) && bStr[i] < bytes1(uint8(97))
                 ) && bStr[i] != bytes1(uint8(32))
-            ) return false;
+            ) revert("LIBSANITIZE_SPECIAL_CHARACTER_ERROR");
         } return true;
     }
 
@@ -56,7 +58,9 @@ library LibSanitize {
         bytes memory bStr = bytes(str);
         uint arrayLength = bStr.length;
 		    for (uint i = 0; i < arrayLength; i++) {
-			     if ((bStr[i] >= bytes1(uint8(65))) && (bStr[i] <= bytes1(uint8(90)))) return false;
+			     if (
+              (bStr[i] >= bytes1(uint8(65))) && (bStr[i] <= bytes1(uint8(90)))
+          ) revert("LIBSANITIZE_LOWERCASE_CHARACTER_ERROR");
 		    } return true;
     }
 
@@ -68,7 +72,9 @@ library LibSanitize {
         bytes memory bStr = bytes(str);
         uint arrayLength = bStr.length;
         for (uint i = 0; i < arrayLength; i++) {
-            if ((bStr[i] >= bytes1(uint8(97))) && (bStr[i] <= bytes1(uint8(122)))) return false;
+            if (
+                (bStr[i] >= bytes1(uint8(97))) && (bStr[i] <= bytes1(uint8(122)))
+            ) revert("LIBSANITIZE_UPPERCASE_CHARACTER_ERROR");
         } return true;
     }
 }
