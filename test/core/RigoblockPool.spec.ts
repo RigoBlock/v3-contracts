abfb5ec9b49f6f75bfda0edd655bd038d005f1a6
gab <gabriele.rigo@gmail.com>
2022-07-26 00:26:16 +0200

    style: minor linting

--- test/core/RigoblockPool.spec.ts
+++ test/core/RigoblockPool.spec.ts
@@ -178,10 +178,7 @@ describe("Proxy", async () => {
             const AuthorityCore = await hre.ethers.getContractFactory("AuthorityCore")
             const authority = AuthorityCore.attach(AuthorityCoreInstance.address)
             //"9e4e93d0": "isValidNav(uint256,uint256,bytes32,bytes)"
-            await authority.addMethod(
-                "0x9e4e93d0",
-                navVerifier.address
-            )
+            await authority.addMethod("0x9e4e93d0", navVerifier.address)
             await expect(
                 pool.setUnitaryValue(
                     newValue,
