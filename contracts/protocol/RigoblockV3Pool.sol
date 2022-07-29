8df845111a07e85721de5b3e7f1de36b776d8c76
gab <gabriele.rigo@gmail.com>
2022-07-29 17:29:18 +0200

    feat: move user actions to abstract subcontract

--- contracts/protocol/RigoblockV3Pool.sol
+++ contracts/protocol/RigoblockV3Pool.sol
@@ -20,7 +20,6 @@
 pragma solidity 0.8.14;
 
 import {IAuthorityCore as Authority} from "./interfaces/IAuthorityCore.sol";
-import {IKyc as Kyc} from "./interfaces/IKyc.sol";
 import {IERC20 as Token} from "./interfaces/IERC20.sol";
 
 import "./IRigoblockV3Pool.sol";
@@ -28,6 +27,7 @@ import "./core/immutable/MixinConstants.sol";
 import "./core/immutable/MixinImmutables.sol";
 import "./core/immutable/MixinStorage.sol";
 import "./core/actions/MixinOwnerActions.sol";
+import "./core/actions/MixinUserActions.sol";
 
 /// @title RigoblockV3Pool - A set of rules for Rigoblock pools.
 /// @author Gabriele Rigo - <gab@rigoblock.com>
@@ -37,7 +37,8 @@ contract RigoblockV3Pool is
     MixinConstants,
     MixinImmutables,
     MixinStorage,
-    MixinOwnerActions
+    MixinOwnerActions,
+    MixinUserActions
 {
 
     // reading immutable through internal method more gas efficient
@@ -55,16 +56,6 @@ contract RigoblockV3Pool is
             size := extcodesize(self)
         }
         require(size == 0, "POOL_ALREADY_INITIALIZED_ERROR");
-        _;
-    }
-
-    modifier hasEnough(uint256 _amount) {
-        require(userAccount[msg.sender].balance >= _amount, "POOL_BURN_NOT_ENOUGH_ERROR");
-        _;
-    }
-
-    modifier minimumPeriodPast() {
-        require(block.timestamp >= userAccount[msg.sender].activation, "POOL_MINIMUM_PERIOD_NOT_ENOUGH_ERROR");
         _;
     }
 
@@ -136,61 +127,6 @@ contract RigoblockV3Pool is
         }
 
         emit PoolInitialized(msg.sender, _owner, _poolName, _poolSymbol);
-    }
-
-    /// @dev Allows a user to mint pool tokens on behalf of an address.
-    /// @param _recipient Address receiving the tokens.
-    /// @param _amountIn Amount of base tokens.
-    /// @return recipientAmount Number of tokens minted to recipient.
-    function mint(address _recipient, uint256 _amountIn) public payable override returns (uint256 recipientAmount) {
-        // require whitelisted user if kyc is enforced
-        if (_isKycEnforced()) {
-            require(Kyc(admin.kycProvider).isWhitelistedUser(_recipient), "POOL_CALLER_NOT_WHITELISTED_ERROR");
-        }
-
-        _assertBiggerThanMinimum(_amountIn);
-
-        if (admin.baseToken == address(0)) {
-            require(msg.value == _amountIn, "POOL_MINT_AMOUNTIN_ERROR");
-        } else {
-            _safeTransferFrom(msg.sender, address(this), _amountIn);
-        }
-
-        uint256 markup = (_amountIn * _getSpread()) / SPREAD_BASE;
-        _amountIn -= markup;
-        uint256 mintedAmount = (_amountIn * 10**decimals()) / _getUnitaryValue();
-        poolData.totalSupply += mintedAmount;
-
-        /// @notice allocate pool token transfers and log events.
-        recipientAmount = _allocateMintTokens(_recipient, mintedAmount);
-    }
-
-    /// @dev Allows a pool holder to burn pool tokens.
-    /// @param _amountIn Number of tokens to burn.
-    /// @return netRevenue Net amount of burnt pool tokens.
-    function burn(uint256 _amountIn)
-        external
-        override
-        nonReentrant
-        hasEnough(_amountIn)
-        minimumPeriodPast
-        returns (uint256 netRevenue)
-    {
-        require(_amountIn > 0, "POOL_BURN_NULL_AMOUNT_ERROR");
-
-        /// @notice allocate pool token transfers and log events.
-        uint256 buntAmount = _allocateBurnTokens(_amountIn);
-        poolData.totalSupply -= buntAmount;
-
-        uint256 markup = (buntAmount * _getSpread()) / SPREAD_BASE;
-        buntAmount -= markup;
-        netRevenue = (buntAmount * _getUnitaryValue()) / 10**decimals();
-
-        if (admin.baseToken == address(0)) {
-            payable(msg.sender).transfer(netRevenue);
-        } else {
-            _safeTransfer(msg.sender, netRevenue);
-        }
     }
 
     /*
@@ -279,7 +215,7 @@ contract RigoblockV3Pool is
     /// @dev Decimals are initialized at proxy creation only if base token not null.
     /// @return Number of decimals.
     /// @notice We use this method to save gas on base currency pools.
-    function decimals() public view override returns (uint8) {
+    function decimals() public view override(IERC20, MixinUserActions) returns (uint8) {
         return poolData.decimals != 0 ? poolData.decimals : _conibaseDecimals;
     }
 
@@ -301,66 +237,6 @@ contract RigoblockV3Pool is
     /*
      * INTERNAL FUNCTIONS
      */
-    /// @dev Allocates tokens to recipient.
-    /// @param _recipient Address of the recipient.
-    /// @param _mintedAmount Value of issued tokens.
-    /// @return recipientAmount Number of new tokens issued to recipient.
-    function _allocateMintTokens(address _recipient, uint256 _mintedAmount) internal returns (uint256 recipientAmount) {
-        /// @notice Each mint on same recipient resets prior activation.
-        /// @notice Lock recipient tokens, max lockup 30 days cannot overflow.
-        unchecked {userAccount[_recipient].activation = uint32(block.timestamp) + _getMinPeriod();}
-
-        if (poolData.transactionFee != uint256(0)) {
-            // TODO: test
-            address feeCollector = (admin.feeCollector != address(0) ? admin.feeCollector : owner);
-
-            if (feeCollector == _recipient) {
-                recipientAmount = _mintedAmount;
-                userAccount[feeCollector].balance += recipientAmount;
-                emit Transfer(address(0), feeCollector, recipientAmount);
-            } else {
-                /// @notice Lock fee tokens as well.
-                unchecked {userAccount[feeCollector].activation = (uint32(block.timestamp) + _getMinPeriod());}
-                uint256 feePool = (_mintedAmount * poolData.transactionFee) / FEE_BASE;
-                recipientAmount = _mintedAmount - feePool;
-                userAccount[feeCollector].balance += feePool;
-                userAccount[_recipient].balance += recipientAmount;
-                emit Transfer(address(0), feeCollector, feePool);
-                emit Transfer(address(0), _recipient, recipientAmount);
-            }
-        } else {
-            recipientAmount = _mintedAmount;
-            userAccount[_recipient].balance += recipientAmount;
-            emit Transfer(address(0), _recipient, recipientAmount);
-        }
-    }
-
-    /// @dev Destroys tokens of holder.
-    /// @param _amountIn Value of tokens to be burnt.
-    /// @return buntAmount Number of net burnt tokens.
-    /// @notice Fee is paid in pool tokens.
-    function _allocateBurnTokens(uint256 _amountIn) internal returns (uint256 buntAmount) {
-        if (poolData.transactionFee != uint256(0)) {
-            address feeCollector = (admin.feeCollector != address(0) ? admin.feeCollector : owner);
-            if (msg.sender == feeCollector) {
-                buntAmount = _amountIn;
-                userAccount[msg.sender].balance -= buntAmount;
-                emit Transfer(msg.sender, address(0), buntAmount);
-            } else {
-                uint256 feePool = (_amountIn * poolData.transactionFee) / FEE_BASE;
-                buntAmount = _amountIn - feePool;
-                userAccount[feeCollector].balance += feePool;
-                userAccount[msg.sender].balance -= buntAmount;
-                emit Transfer(msg.sender, feeCollector, feePool);
-                emit Transfer(msg.sender, address(0), buntAmount);
-            }
-        } else {
-            buntAmount = _amountIn;
-            userAccount[msg.sender].balance -= buntAmount;
-            emit Transfer(msg.sender, address(0), buntAmount);
-        }
-    }
-
     /// @dev Returns the address of the application adapter.
     /// @param _selector Hash of the method signature.
     /// @return Address of the application adapter.
@@ -372,43 +248,15 @@ contract RigoblockV3Pool is
         require(address(this) != _implementation, "POOL_IMPLEMENTATION_DIRECT_CALL_NOT_ALLOWED_ERROR");
     }
 
-    function _assertBiggerThanMinimum(uint256 _amount) private view {
-        require(_amount >= 10**decimals() / MINIMUM_ORDER_DIVISOR, "POOL_AMOUNT_SMALLER_THAN_MINIMUM_ERROR");
-    }
-
-    function _getMinPeriod() private view returns (uint32) {
+    function _getMinPeriod() internal view override returns (uint32) {
         return poolData.minPeriod != 0 ? poolData.minPeriod : MIN_LOCKUP;
     }
 
-    function _getSpread() private view returns (uint256) {
+    function _getSpread() internal view override returns (uint256) {
         return poolData.spread != 0 ? poolData.spread : INITIAL_SPREAD;
     }
 
-    function _getUnitaryValue() internal view override returns (uint256) {
+    function _getUnitaryValue() internal view override(MixinUserActions, MixinOwnerActions) returns (uint256) {
         return poolData.unitaryValue != 0 ? poolData.unitaryValue : _coinbaseUnitaryValue;
-    }
-
-    function _isKycEnforced() private view returns (bool) {
-        return admin.kycProvider != address(0);
-    }
-
-    function _safeTransfer(address _to, uint256 _amount) private {
-        // solhint-disable-next-line avoid-low-level-calls
-        // TODO: we may want to use assembly here
-        (bool success, bytes memory data) =
-            admin.baseToken.call(abi.encodeWithSelector(TRANSFER_SELECTOR, _to, _amount));
-        require(success && (data.length == 0 || abi.decode(data, (bool))), "POOL_TRANSFER_FROM_FAILED_ERROR");
-    }
-
-    function _safeTransferFrom(
-        address _from,
-        address _to,
-        uint256 _amount
-    ) private {
-        // solhint-disable-next-line avoid-low-level-calls
-        // TODO: we may want to use assembly here
-        (bool success, bytes memory data) =
-            admin.baseToken.call(abi.encodeWithSelector(TRANSFER_FROM_SELECTOR, _from, _to, _amount));
-        require(success && (data.length == 0 || abi.decode(data, (bool))), "POOL_TRANSFER_FROM_FAILED_ERROR");
     }
 }
