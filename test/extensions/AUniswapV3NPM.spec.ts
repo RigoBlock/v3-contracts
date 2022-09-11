d61bccb099c942207a7cf0d6f7d599f7fd8907e5
gab <gabriele.rigo@gmail.com>
2022-09-11 17:12:44 +0200

    add initial uniswap npm adapter test file

---
+++ test/extensions/AUniswapV3NPM.spec.ts
@@ -0,0 +1,89 @@
+import { expect } from "chai";
+import hre, { deployments, waffle, ethers } from "hardhat";
+import "@nomiclabs/hardhat-ethers";
+import { AddressZero } from "@ethersproject/constants";
+import { parseEther } from "@ethersproject/units";
+import { BigNumber, Contract } from "ethers";
+import { calculateProxyAddress, calculateProxyAddressWithCallback } from "../../src/utils/proxies";
+import { timeTravel } from "../utils/utils";
+import { getAddress } from "ethers/lib/utils";
+
+describe("AUniswapV3NPM", async () => {
+    const [ user1, user2 ] = waffle.provider.getWallets()
+
+    const setupTests = deployments.createFixture(async ({ deployments }) => {
+        // TODO: check if shoud create custom fixture with less contracts initialization
+        await deployments.fixture('tests-setup')
+        const RigoblockPoolProxyFactory = await deployments.get("RigoblockPoolProxyFactory")
+        const Factory = await hre.ethers.getContractFactory("RigoblockPoolProxyFactory")
+        const GrgTokenInstance = await deployments.get("RigoToken")
+        const GrgToken = await hre.ethers.getContractFactory("RigoToken")
+        const AuthorityCoreInstance = await deployments.get("AuthorityCore")
+        const AuthorityCore = await hre.ethers.getContractFactory("AuthorityCore")
+        const authority = AuthorityCore.attach(AuthorityCoreInstance.address)
+        const AUniswapV3NPMInstance = await deployments.get("AUniswapV3NPM")
+        await authority.setAdapter(AUniswapV3NPMInstance.address, true)
+        // "88316456": "mint((address,address,uint24,int24,int24,uint256,uint256,uint256,uint256,address,uint256))",
+        // "c20ec580": "UNISWAP_V3_NPM_ADDRESS()",
+        // "a785a3d8": "WethAddress()",
+        // "42966c68": "burn(uint256)",
+        // "fc6f7865": "collect((uint256,address,uint128,uint128))",
+        // "13ead562": "createAndInitializePoolIfNecessary(address,address,uint24,uint160)",
+        // "0c49ccbe": "decreaseLiquidity((uint256,uint128,uint256,uint256,uint256))",
+        // "219f5d17": "increaseLiquidity((uint256,uint256,uint256,uint256,uint256,uint256))",
+        // "12210e8a": "refundETH()",
+        // "df2ab5bb": "sweepToken(address,uint256,address)",
+        // "49404b7c": "unwrapWETH9(uint256,address)",
+        // "1c58db4f": "wrapETH(uint256)"
+        await authority.addMethod("0x88316456", AUniswapV3NPMInstance.address)
+        await authority.addMethod("0xc20ec580", AUniswapV3NPMInstance.address)
+        await authority.addMethod("0xa785a3d8", AUniswapV3NPMInstance.address)
+        await authority.addMethod("0x42966c68", AUniswapV3NPMInstance.address)
+        await authority.addMethod("0xfc6f7865", AUniswapV3NPMInstance.address)
+        await authority.addMethod("0x13ead562", AUniswapV3NPMInstance.address)
+        await authority.addMethod("0x0c49ccbe", AUniswapV3NPMInstance.address)
+        await authority.addMethod("0x219f5d17", AUniswapV3NPMInstance.address)
+        await authority.addMethod("0x12210e8a", AUniswapV3NPMInstance.address)
+        await authority.addMethod("0xdf2ab5bb", AUniswapV3NPMInstance.address)
+        await authority.addMethod("0x49404b7c", AUniswapV3NPMInstance.address)
+        await authority.addMethod("0x1c58db4f", AUniswapV3NPMInstance.address)
+        const factory = Factory.attach(RigoblockPoolProxyFactory.address)
+        const { newPoolAddress, poolId } = await factory.callStatic.createPool(
+            'testpool',
+            'TEST',
+            AddressZero
+        )
+        await factory.createPool('testpool','TEST',AddressZero)
+        return {
+            grgToken: GrgToken.attach(GrgTokenInstance.address),
+            newPoolAddress,
+            poolId
+        }
+    })
+
+    describe("mint", async () => {
+        it('should mint an NFT', async () => {
+            const { grgToken, newPoolAddress, poolId } = await setupTests()
+            const Pool = await hre.ethers.getContractFactory("AUniswapV3NPM")
+            const pool = Pool.attach(newPoolAddress)
+            const amount = parseEther("100")
+            // we send both Ether and GRG to the pool
+            await user1.sendTransaction({ to: newPoolAddress, value: amount})
+            await grgToken.transfer(newPoolAddress, amount)
+            await pool.createAndInitializePoolIfNecessary(grgToken.address, grgToken.address, 1, 1)
+            await pool.mint({
+                token0: grgToken.address,
+                token1: grgToken.address,
+                fee: 10,
+                tickLower: 1,
+                tickUpper: 200,
+                amount0Desired: 100,
+                amount1Desired: 100,
+                amount0Min: 0,
+                amount1Min: 0,
+                recipient: pool.address,
+                deadline: 1
+            })
+        })
+    })
+})
