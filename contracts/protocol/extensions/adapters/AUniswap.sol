5c99829d54545024f46b0a61f8301c80ab3e5307
gab <gabriele.rigo@gmail.com>
2022-09-15 12:43:26 +0200

    fix: safe approve gas optimization and uniswap adapter minor linting
    - remove definition of return variable success
    - assert instead of require (approval will never fail unless rogue token, plus we already read returned data)
    - hardcode approve selector instead of reading from storage

--- contracts/protocol/extensions/adapters/AUniswap.sol
+++ contracts/protocol/extensions/adapters/AUniswap.sol
@@ -32,8 +32,6 @@ contract AUniswap is AUniswapV3NPM {
     using Path for bytes;
 
     // storage must be immutable as needs to be rutime consistent
-    bytes4 private constant APPROVE_SELECTOR = bytes4(keccak256(bytes("approve(address,uint256)")));
-
     // 0xE592427A0AEce92De3Edee1F18E0157C05861564 on public networks
     address public immutable UNISWAP_SWAP_ROUTER_2_ADDRESS;
 
@@ -51,14 +49,14 @@ contract AUniswap is AUniswapV3NPM {
     /*
      * UNISWAP V2 METHODS
     */
-    /// @notice Swaps `amountIn` of one token for as much as possible of another token
+    /// @notice Swaps `amountIn` of one token for as much as possible of another token.
     /// @dev Setting `amountIn` to 0 will cause the contract to look up its own balance,
     /// and swap the entire amount, enabling contracts to send tokens before calling this function.
-    /// @param amountIn The amount of token to swap
-    /// @param amountOutMin The minimum amount of output that must be received
-    /// @param path The ordered list of tokens to swap through
-    /// @param to The recipient address
-    /// @return amountOut The amount of the received token
+    /// @param amountIn The amount of token to swap.
+    /// @param amountOutMin The minimum amount of output that must be received.
+    /// @param path The ordered list of tokens to swap through.
+    /// @param to The recipient address.
+    /// @return amountOut The amount of the received token.
     function swapExactTokensForTokens(
         uint256 amountIn,
         uint256 amountOutMin,
@@ -73,12 +71,12 @@ contract AUniswap is AUniswapV3NPM {
         );
     }
 
-    /// @notice Swaps as little as possible of one token for an exact amount of another token
-    /// @param amountOut The amount of token to swap for
-    /// @param amountInMax The maximum amount of input that the caller will pay
-    /// @param path The ordered list of tokens to swap through
-    /// @param to The recipient address
-    /// @return amountIn The amount of token to pay
+    /// @notice Swaps as little as possible of one token for an exact amount of another token.
+    /// @param amountOut The amount of token to swap for.
+    /// @param amountInMax The maximum amount of input that the caller will pay.
+    /// @param path The ordered list of tokens to swap through.
+    /// @param to The recipient address.
+    /// @return amountIn The amount of token to pay.
     function swapTokensForExactTokens(
         uint256 amountOut,
         uint256 amountInMax,
@@ -96,9 +94,9 @@ contract AUniswap is AUniswapV3NPM {
     /*
      * UNISWAP V3 SWAP METHODS
     */
-    /// @notice Swaps `amountIn` of one token for as much as possible of another token
-    /// @param params The parameters necessary for the swap, encoded as `ExactInputSingleParams` in memory
-    /// @return amountOut The amount of the received token
+    /// @notice Swaps `amountIn` of one token for as much as possible of another token.
+    /// @param params The parameters necessary for the swap, encoded as `ExactInputSingleParams` in memory.
+    /// @return amountOut The amount of the received token.
     function exactInputSingle(ISwapRouter02.ExactInputSingleParams calldata params)
         external
         returns (uint256 amountOut)
@@ -123,9 +121,9 @@ contract AUniswap is AUniswapV3NPM {
         _safeApprove(params.tokenIn, _getUniswapRouter2(), uint256(1));
     }
 
-    /// @notice Swaps `amountIn` of one token for as much as possible of another along the specified path
-    /// @param params The parameters necessary for the multi-hop swap, encoded as `ExactInputParams` in memory
-    /// @return amountOut The amount of the received token
+    /// @notice Swaps `amountIn` of one token for as much as possible of another along the specified path.
+    /// @param params The parameters necessary for the multi-hop swap, encoded as `ExactInputParams` in memory.
+    /// @return amountOut The amount of the received token.
     function exactInput(ISwapRouter02.ExactInputParams calldata params) external returns (uint256 amountOut) {
         (address tokenIn, , ) = params.path.decodeFirstPool();
 
@@ -146,9 +144,9 @@ contract AUniswap is AUniswapV3NPM {
         _safeApprove(tokenIn, _getUniswapRouter2(), uint256(1));
     }
 
-    /// @notice Swaps as little as possible of one token for `amountOut` of another token
-    /// @param params The parameters necessary for the swap, encoded as `ExactOutputSingleParams` in memory
-    /// @return amountIn The amount of the input token
+    /// @notice Swaps as little as possible of one token for `amountOut` of another token.
+    /// @param params The parameters necessary for the swap, encoded as `ExactOutputSingleParams` in memory.
+    /// @return amountIn The amount of the input token.
     function exactOutputSingle(ISwapRouter02.ExactOutputSingleParams calldata params)
         external
         returns (uint256 amountIn)
@@ -173,9 +171,9 @@ contract AUniswap is AUniswapV3NPM {
         _safeApprove(params.tokenIn, _getUniswapRouter2(), uint256(1));
     }
 
-    /// @notice Swaps as little as possible of one token for `amountOut` of another along the specified path (reversed)
-    /// @param params The parameters necessary for the multi-hop swap, encoded as `ExactOutputParams` in memory
-    /// @return amountIn The amount of the input token
+    /// @notice Swaps as little as possible of one token for `amountOut` of another along the specified path (reversed).
+    /// @param params The parameters necessary for the multi-hop swap, encoded as `ExactOutputParams` in memory.
+    /// @return amountIn The amount of the input token.
     function exactOutput(ISwapRouter02.ExactOutputParams calldata params) external returns (uint256 amountIn) {
         (address tokenIn, , ) = params.path.decodeFirstPool();
 
@@ -213,11 +211,11 @@ contract AUniswap is AUniswapV3NPM {
         );
     }
 
-    /// @notice Transfers the full amount of a token held by this contract to recipient
-    /// @dev The amountMinimum parameter prevents malicious contracts from stealing the token from users
-    /// @param token The contract address of the token which will be transferred to `recipient`
-    /// @param amountMinimum The minimum amount of token required for a transfer
-    /// @param recipient The destination address of the token
+    /// @notice Transfers the full amount of a token held by this contract to recipient.
+    /// @dev The amountMinimum parameter prevents malicious contracts from stealing the token from users.
+    /// @param token The contract address of the token which will be transferred to `recipient`.
+    /// @param amountMinimum The minimum amount of token required for a transfer.
+    /// @param recipient The destination address of the token.
     function sweepToken(
         address token,
         uint256 amountMinimum,
@@ -231,8 +229,8 @@ contract AUniswap is AUniswapV3NPM {
     }
 
     /// @notice Transfers the full amount of a token held by this contract to recipient, with a percentage between
-    /// 0 (exclusive) and 1 (inclusive) going to feeRecipient
-    /// @dev The amountMinimum parameter prevents malicious contracts from stealing the token from users
+    /// 0 (exclusive) and 1 (inclusive) going to feeRecipient.
+    /// @dev The amountMinimum parameter prevents malicious contracts from stealing the token from users.
     function sweepTokenWithFee(
         address token,
         uint256 amountMinimum,
@@ -279,7 +277,7 @@ contract AUniswap is AUniswapV3NPM {
     }
 
     /// @notice Unwraps the contract's WETH9 balance and sends it to recipient as ETH, with a percentage between
-    /// 0 (exclusive), and 1 (inclusive) going to feeRecipient
+    /// 0 (exclusive), and 1 (inclusive) going to feeRecipient.
     /// @dev The amountMinimum parameter prevents malicious contracts from stealing WETH9 from users.
     function unwrapWETH9WithFee(
         uint256 amountMinimum,
@@ -288,7 +286,7 @@ contract AUniswap is AUniswapV3NPM {
     ) external virtual {}
 
     /// @notice Unwraps the contract's WETH9 balance and sends it to recipient as ETH, with a percentage between
-    /// 0 (exclusive), and 1 (inclusive) going to feeRecipient
+    /// 0 (exclusive), and 1 (inclusive) going to feeRecipient.
     /// @dev The amountMinimum parameter prevents malicious contracts from stealing WETH9 from users.
     function unwrapWETH9WithFee(
         uint256 amountMinimum,
@@ -315,14 +313,11 @@ contract AUniswap is AUniswapV3NPM {
         address spender,
         uint256 value
     ) internal override {
+        // 0x095ea7b3 = bytes4(keccak256(bytes("approve(address,uint256)")))
         // solhint-disable-next-line avoid-low-level-calls
-        // TODO: we may want to use assembly here
-        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(_getApproveSelector(), spender, value));
-        require(success && (data.length == 0 || abi.decode(data, (bool))), "AUNISWAPV3_TOKEN_APPROVE_FAILED_ERROR");
-    }
-
-    function _getApproveSelector() private pure returns (bytes4) {
-        return APPROVE_SELECTOR;
+        ( , bytes memory data) = token.call(abi.encodeWithSelector(0x095ea7b3, spender, value));
+        // approval never fails unless rogue token
+        assert(data.length == 0 || abi.decode(data, (bool)));
     }
 
     function _getUniswapNpmAddress() internal view override returns (address) {
