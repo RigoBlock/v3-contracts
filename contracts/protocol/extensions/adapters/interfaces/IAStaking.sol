d2991ce7941d78537736b51521f0639eff14a1f7
gab <gabriele.rigo@gmail.com>
2022-09-19 13:46:14 +0200

    fix: updated staking adapter interface with new methods

--- contracts/protocol/extensions/adapters/interfaces/IAStaking.sol
+++ contracts/protocol/extensions/adapters/interfaces/IAStaking.sol
@@ -5,4 +5,10 @@ pragma solidity >=0.8.0 <0.9.0;
 interface IAStaking {
     /// @notice Creating staking pool if doesn't exist effectively locks direct call.
     function stake(uint256 _amount) external;
+
+    function undelegateStake(uint256 _amount) external;
+
+    function unstake(uint256 _amount) external;
+
+    function withdrawDelegatorRewards() external;
 }
