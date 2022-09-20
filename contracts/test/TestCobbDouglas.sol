e20eceb802cea5f3116fed66c93d0e786b6b5fa3
gab <gabriele.rigo@gmail.com>
2022-09-20 19:16:54 +0200

    ci: add cobb douglas test contract

---
+++ contracts/test/TestCobbDouglas.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: Apache 2.0
+
+pragma solidity >0.7.0 <0.9.0;
+
+import "../staking/libs/LibCobbDouglas.sol";
+
+contract TestCobbDouglas {
+
+    function cobbDouglas(
+        uint256 totalRewards,
+        uint256 fees,
+        uint256 totalFees,
+        uint256 stake,
+        uint256 totalStake,
+        uint32 alphaNumerator,
+        uint32 alphaDenominator
+    )
+        external
+        pure
+        returns (uint256 rewards)
+    {
+        rewards = LibCobbDouglas.cobbDouglas(
+            totalRewards,
+            fees,
+            totalFees,
+            stake,
+            totalStake,
+            alphaNumerator,
+            alphaDenominator
+        );
+    }
+}
