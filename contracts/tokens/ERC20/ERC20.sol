05c4f2ed5427092728a82bbbfd3279aaccf050af
gab <gabriele.rigo@gmail.com>
2022-09-22 16:41:50 +0200

    fix: fix GRG token overrides
    - added overrides where missing
    - import IGRG inherits IERC20
    - fixed ERC20 overrides
    - fixed IERC20 missing method totalSupply()

--- contracts/tokens/ERC20/ERC20.sol
+++ contracts/tokens/ERC20/ERC20.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.0 <0.9.0;
 import {IERC20} from "./IERC20.sol";
 
 abstract contract ERC20 is IERC20 {
-    function transfer(address _to, uint256 _value) external returns (bool success) {
+    function transfer(address _to, uint256 _value) external override returns (bool success) {
         require(balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]);
         balances[msg.sender] -= _value;
         balances[_to] += _value;
@@ -16,7 +16,7 @@ abstract contract ERC20 is IERC20 {
         address _from,
         address _to,
         uint256 _value
-    ) external virtual returns (bool success) {
+    ) external virtual override returns (bool success) {
         require(
             balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]
         );
@@ -27,21 +27,21 @@ abstract contract ERC20 is IERC20 {
         return true;
     }
 
-    function approve(address _spender, uint256 _value) external returns (bool success) {
+    function approve(address _spender, uint256 _value) external override returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    function balanceOf(address _owner) external view returns (uint256) {
+    function balanceOf(address _owner) external view override returns (uint256) {
         return balances[_owner];
     }
 
-    function allowance(address _owner, address _spender) external view returns (uint256) {
+    function allowance(address _owner, address _spender) external view override returns (uint256) {
         return allowed[_owner][_spender];
     }
 
-    uint256 public totalSupply;
+    uint256 public override totalSupply;
     mapping(address => uint256) internal balances;
     mapping(address => mapping(address => uint256)) internal allowed;
 }
