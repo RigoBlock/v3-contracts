2fdef5587f090875429cf3ec4322a3847481760f
gab <gabriele.rigo@gmail.com>
2022-10-08 18:04:40 +0200

    docs: add natspec docs to utils/owned/* contracts

--- contracts/utils/owned/OwnedUninitialized.sol
+++ contracts/utils/owned/OwnedUninitialized.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 import {IOwnedUninitialized} from "./IOwnedUninitialized.sol";
 
 abstract contract OwnedUninitialized is IOwnedUninitialized {
+    /// @inheritdoc IOwnedUninitialized
     address public override owner;
 
     modifier onlyOwner() {
@@ -11,6 +12,7 @@ abstract contract OwnedUninitialized is IOwnedUninitialized {
         _;
     }
 
+    /// @inheritdoc IOwnedUninitialized
     function setOwner(address _new) public override onlyOwner {
         require(_new != address(0));
         address old = owner;
