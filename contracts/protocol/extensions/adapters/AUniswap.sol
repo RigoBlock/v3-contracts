e9b02942b79a3ac893524ec89d1befa30fb3102d
gab <gabriele.rigo@gmail.com>
2022-10-25 16:57:42 +0200

    docs: update comment in uniswap adapter

--- contracts/protocol/extensions/adapters/AUniswap.sol
+++ contracts/protocol/extensions/adapters/AUniswap.sol
@@ -36,7 +36,7 @@ contract AUniswap is IAUniswap, AUniswapV3NPM {
     using Path for bytes;
 
     // storage must be immutable as needs to be rutime consistent
-    // 0xE592427A0AEce92De3Edee1F18E0157C05861564 on public networks
+    // 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45 on public networks
     /// @inheritdoc IAUniswap
     address public immutable override uniswapRouter02;
 
