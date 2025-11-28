fda9e9ebb1a0e3a1dd5e355d76303fbe7533e4c8
Gabriele Rigo <gabriele.rigo@gmail.com>
2025-11-28 12:57:43 +0100

    fix: smart pool license

--- contracts/protocol/core/immutable/MixinStorage.sol
+++ contracts/protocol/core/immutable/MixinStorage.sol
@@ -1,27 +1,9 @@
-// SPDX-License-Identifier: Apache 2.0
-/*
-
- Copyright 2022-2025 Rigo Intl.
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-
-*/
+// SPDX-License-Identifier: Apache 2.0-or-later
+pragma solidity >=0.8.0 <0.9.0;
 
 import {MixinImmutables} from "./MixinImmutables.sol";
 import {AddressSet, Pool} from "../../libraries/EnumerableSet.sol";
 import {ApplicationsSlot} from "../../libraries/ApplicationsLib.sol";
-
-pragma solidity >=0.8.0 <0.9.0;
 
 /// @notice Storage slots must be preserved to prevent storage clashing.
 /// @dev Pool storage is not sequential: each variable is wrapped into a struct which is assigned a storage slot.
