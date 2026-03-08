62159b7968281bb8e1b27c6da516a3ee2fc39c7f
Gabriele Rigo <gabriele.rigo@gmail.com>
2026-03-08 23:18:26 +0100

    docs lint

--- contracts/protocol/extensions/adapters/interfaces/IA0xRouter.sol
+++ contracts/protocol/extensions/adapters/interfaces/IA0xRouter.sol
@@ -42,10 +42,7 @@ interface IA0xRouter {
     error InsufficientNativeBalance();
 
     /// @notice Thrown when a settler action is not in the adapter's allowlist.
-    /// @dev Only whitelisted DEX swap actions are permitted. BASIC (arbitrary calldata to arbitrary
-    ///  target), RFQ (arbitrary off-chain pricing), RENEGADE (arbitrary target), and METATXN_*
-    ///  (wrong execution flow) are blocked. Unrecognized selectors are also blocked by default,
-    ///  providing forward security when 0x adds new action types.
+    /// @dev Only whitelisted DEX swap actions are permitted.
     /// @param actionSelector The 4-byte selector of the rejected action.
     error ActionNotAllowed(bytes4 actionSelector);
 
