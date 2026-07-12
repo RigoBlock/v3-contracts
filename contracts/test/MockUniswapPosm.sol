3319876df8a8f418f57ae4b158e1370fac3945f0
Gabriele Rigo <gabriele.rigo@gmail.com>
2026-07-12 16:51:00 +0200

    chore: remove deprecated solc versions

--- contracts/test/MockUniswapPosm.sol
+++ contracts/test/MockUniswapPosm.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache 2.0
 
-pragma solidity 0.8.24;
+pragma solidity 0.8.26;
 
 import {Position} from "@uniswap/v4-core/src/libraries/Position.sol";
 import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
